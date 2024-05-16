d186798d3269661c884d0e79351c0a1779dbbf1e
xuanhau0112 <tranxuanhau321@gmail.com>
2024-05-17 00:45:03 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,10 @@
+$('#slide').owlCarousel({
+loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:false,
+	dots:false,// cho xuất hiện bộ nút tới lui 
+})
+// JavaScript Document
